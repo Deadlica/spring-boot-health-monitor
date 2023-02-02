050ebe4df5d88279c6ed88afaf641b7ff7ef2053
Deadlica <samuelgreenberg00@gmail.com>
2023-02-02 02:32:51 +0100

    Controller updated

--- health-monitor/src/main/java/com/samuel/healthmonitor/Controller.java
+++ health-monitor/src/main/java/com/samuel/healthmonitor/Controller.java
@@ -1,6 +1,7 @@
 package com.samuel.healthmonitor;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,17 @@ public class Controller {
     @GetMapping("/monitor")
     public String monitor() {
         return "Hello World";
+    }
+
+    @GetMapping("/monitor/cpu-overload")
+    public String cpuOverload() {
+        try {
+            while(true) {
+                Runnable runner = () -> {    while(true) {}    };
+                new Thread(runner).start();
+                Thread.sleep(5000);
+            }
+        } catch(Exception e) {}
+        return "CPU fried :(";
     }
 }
